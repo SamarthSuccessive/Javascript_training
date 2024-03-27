825a66dc4b5215c8122d90e610fb95624ecdaf86
SamarthSuccessive <samarth.srivastava@successive.tech>
2024-03-27 16:57:50 +0530

    Assignment 9

---
+++ Assignment 9/Ques1.js
@@ -0,0 +1,8 @@
+// Install the lodash package using npm install lodash. and use any of its function
+
+//We can install the lodash by the following command npm i lodash
+ 
+const load= require("lodash");
+// Use of .add() method 
+let ans = load.add(12, 38);
+console.log(ans)
